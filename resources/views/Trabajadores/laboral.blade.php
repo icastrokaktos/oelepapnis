312c97051a6c4ea564549f26df20573cefefab8d
Ivan Castro <icastro@kaktos.cl>
2017-11-02 10:00:27 -0400

    Vistas agregadas

---
+++ resources/views/Trabajadores/laboral.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	Principal
+@endsection
+
+@section('contentheader_title')
+	Cuadro Resumen
+@endsection
+
+@section('contentheader_description')
+	Sin Papeleo v1.0
+@endsection
+
+@section('main-content')
+
+@endsection
